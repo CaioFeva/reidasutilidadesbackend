18fb4f295d043d497592b228b5a96869b1fc6c00
Caio César <caio.cesaralves90@gmail.com>
2024-11-04 20:14:03 -0300

    commit

--- server.js
+++ server.js
@@ -11,15 +11,39 @@ app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  //   ssl: {
+  //     rejectUnauthorized: false
+  //   }
 });
 
 pool
   .connect()
   .then(() => console.log("Conectado ao banco de dados PostgreSQL"))
   .catch((err) => console.error("Erro ao conectar ao banco de dados", err));
+
+app.get("/api/produto/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM produto WHERE id = $1", [
+      id,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Produto não encontrado" });
+    }
+
+    const produto = result.rows[0];
+    const produtoFormatado = {
+      ...produto,
+      price: produto.price ? parseFloat(produto.price) : 0.0,
+    };
+
+    res.json(produtoFormatado);
+  } catch (error) {
+    console.error("Erro ao buscar produto:", error);
+    res.status(500).json({ error: "Erro ao buscar produto" });
+  }
+});
 
 app.get("/api/produtos", async (req, res) => {
   try {
@@ -35,36 +59,56 @@ app.get("/api/produtos", async (req, res) => {
   }
 });
 
-app.post('/api/produtos', async (req, res) => {
-    const { name, description, price, images, dimensions, weight, stock, category } = req.body;
-  
-    // Validação básica dos campos obrigatórios
-    if (!name || !price || !stock) {
-      return res.status(400).json({ error: 'Os campos name, price e stock são obrigatórios' });
-    }
-  
-    try {
-      const query = `
+app.post("/api/produtos", async (req, res) => {
+  const {
+    name,
+    description,
+    price,
+    images,
+    dimensions,
+    weight,
+    stock,
+    category,
+  } = req.body;
+
+  // Validação básica dos campos obrigatórios
+  if (!name || !price || !stock) {
+    return res
+      .status(400)
+      .json({ error: "Os campos name, price e stock são obrigatórios" });
+  }
+
+  try {
+    const query = `
         INSERT INTO produto (name, description, price, images, dimensions, weight, stock, category)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
         RETURNING *;
       `;
-  
-      const values = [name, description, price, images, dimensions, weight, stock, category];
-  
-      // Executa o comando INSERT e retorna o produto inserido
-      const result = await pool.query(query, values);
-      const newProduct = result.rows[0];
-  
-      res.status(201).json(newProduct); // Retorna o produto inserido com status 201 (Created)
-    } catch (error) {
-      console.error('Erro ao inserir produto:', error);
-      res.status(500).json({ error: 'Erro ao inserir produto' });
-    }
-  });
+
+    const values = [
+      name,
+      description,
+      price,
+      images,
+      dimensions,
+      weight,
+      stock,
+      category,
+    ];
+
+    // Executa o comando INSERT e retorna o produto inserido
+    const result = await pool.query(query, values);
+    const newProduct = result.rows[0];
+
+    res.status(201).json(newProduct); // Retorna o produto inserido com status 201 (Created)
+  } catch (error) {
+    console.error("Erro ao inserir produto:", error);
+    res.status(500).json({ error: "Erro ao inserir produto" });
+  }
+});
 
 // Endpoint PUT para atualizar um produto existente
-app.put("/api/produtos/:id", (req, res) => {
+app.put("/api/produtos/:id", async (req, res) => {
   const { id } = req.params;
   const {
     name,
@@ -77,25 +121,45 @@ app.put("/api/produtos/:id", (req, res) => {
     category,
   } = req.body;
 
-  // Encontra o produto pelo ID
-  const produtoIndex = produtos.findIndex((produto) => produto.id === id);
-  if (produtoIndex === -1) {
-    return res.status(404).json({ error: "Produto não encontrado" });
+  try {
+    // Verifica se o produto existe
+    const result = await pool.query("SELECT * FROM produto WHERE id = $1", [
+      id,
+    ]);
+
+    console.log("result", result);
+    
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Produto não encontrado" });
+    }
+
+    // Atualiza o produto
+    const updateQuery = `
+      UPDATE produto
+      SET name = $1, description = $2, price = $3, images = $4, dimensions = $5, 
+          weight = $6, stock = $7, category = $8
+      WHERE id = $9
+      RETURNING *
+    `;
+    const values = [
+      name,
+      description,
+      price,
+      images,
+      dimensions,
+      weight,
+      stock,
+      category,
+      id,
+    ];
+    const updatedProduct = await pool.query(updateQuery, values);
+
+    // Retorna o produto atualizado
+    res.json(updatedProduct.rows[0]);
+  } catch (error) {
+    console.error("Erro ao atualizar produto:", error);
+    res.status(500).json({ error: "Erro ao atualizar produto" });
   }
-
-  produtos[produtoIndex] = {
-    ...produtos[produtoIndex],
-    name,
-    description,
-    price,
-    images,
-    dimensions,
-    weight,
-    stock,
-    category,
-  };
-
-  res.json(produtos[produtoIndex]);
 });
 
 // Endpoint DELETE para excluir um produto
